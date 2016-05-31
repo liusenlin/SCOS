1b76bf1495d91fcf564555f8834774760908a933
insomniasky <xunaiting_better@163.com>
2016-05-31 20:16:02 +0800

    5/31/20.15

--- app/src/main/java/es/source/code/activity/MainScreen.java
+++ app/src/main/java/es/source/code/activity/MainScreen.java
@@ -3,11 +3,11 @@ package es.source.code.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SCOSEntry extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.entry);
+        setContentView(R.layout.activity_main_screen);
     }
 }
