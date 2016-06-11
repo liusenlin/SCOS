bb7b8fc55d31879dc8eebf0278f1dcbc4641dca9
insomniasky <xunaiting_better@163.com>
2016-06-11 14:14:38 +0800

    2016/6/11

--- app/src/main/java/es/source/code/activity/MainScreen.java
+++ app/src/main/java/es/source/code/activity/MainScreen.java
@@ -1,13 +1,27 @@
 package es.source.code.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        Intent intent = getIntent();
+        //从Intent当中根据key取得value
+        String value = intent.getStringExtra("StoM");
+        if(value != "FromEntry"){
+           Button button = (Button)findViewById(R.id.radio_button1);
+            button.setVisibility(View.GONE);
+            button = (Button)findViewById(R.id.radio_button2);
+            button.setVisibility(View.GONE);
+        }
+
     }
 }
